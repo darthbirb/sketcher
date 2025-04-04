c468003eeff792f16fa7f61f965b9c74997b28fb
darthbirb <97291256+darthbirb@users.noreply.github.com>
2025-04-04 19:59:01 +0200

    Set up Tailwind and shadcn

--- web-app/frontend/vite.config.ts
+++ web-app/frontend/vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import path from "path"
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
@@ -8,6 +9,11 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     proxy: {
       '/predict': 'http://127.0.0.1:5000', // proxy backend request for /predict to your Flask server
