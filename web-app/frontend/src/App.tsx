e50e00ad0b8c1041c9b11120a1329fda9acd07b7
darthbirb <97291256+darthbirb@users.noreply.github.com>
2025-04-05 14:00:22 +0200

    Shadcn setup, added button

--- web-app/frontend/src/App.tsx
+++ web-app/frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 // @ts-ignore
 import SketchCanvas from './components/SketchCanvas';
+import { Button } from "@/components/ui/button";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <li>Tab 2</li>
           <li>Tab 3</li>
         </ul>
+        <Button variant="outline" className="mt-4">Click Me</Button>
       </div>
 
       {/* Main content area */}
